559ada4a96438bcfc198e8b073884cb4ad56eca8
huh354 <143356635+huh354@users.noreply.github.com>
2023-09-18 00:10:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h1>Welcome to my card</h1>
+        <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
+        <div className="paragraph">
+          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
+        </div>
+        <div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button> </a> </div>
+      </div>
+    );
+  }
+
+export default App;
